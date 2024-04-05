038b9e5b6464cd6563d753de2bca9421d002be42
ethpalser <eth.palser@gmail.com>
2024-04-05 12:49:10 -0400

    added tests for normalizeURL function

---
+++ crawl.test.js
@@ -0,0 +1,38 @@
+const { test, expect } = require('@jest/globals')
+const { normalizeURL } = require('./crawl.js')
+
+test('https://www.google.com/ normalize to www.google.com', () => {
+    expect(normalizeURL('https://www.google.com/')).toBe('www.google.com')
+})
+
+test('https://www.google.com normalize to www.google.com', () => {
+    expect(normalizeURL('https://www.google.com/')).toBe('www.google.com')
+})
+
+test('http://www.google.com/ normalize to www.google.com', () => {
+    expect(normalizeURL('https://www.google.com/')).toBe('www.google.com')
+})
+
+test('http://www.google.com normalize to www.google.com', () => {
+    expect(normalizeURL('https://www.google.com/')).toBe('www.google.com')
+})
+
+test('www.google.com normalize to www.google.com', () => {
+    expect(normalizeURL('https://www.google.com/')).toBe('www.google.com')
+})
+
+test('https://www.google.com/account normalize to www.google.com/account', () => {
+    expect(normalizeURL('https://www.google.com/account')).toBe('www.google.com/account')
+})
+
+test('https://www.google.com/account?sort=name normalize to www.google.com/account', () => {
+    expect(normalizeURL('https://www.google.com/account?sort=name')).toBe('www.google.com/account')
+})
+
+test('emailto:www.google.com normalize to www.google.com', () => {
+    expect(normalizeURL('emailto:www.google.com')).toBe('www.google.com')
+})
+
+test('google.com normalize to www.google.com', () => {
+    expect(normalizeURL('google.com')).toBe('www.google.com')
+})
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log('hello world')
+
+module.exports = {
+    normalizeURL
+}
