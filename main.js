8d73fb52f3b7183f0a83a70792833ac9f208f673
ethpalser <eth.palser@gmail.com>
2024-04-06 09:49:18 -0400

    implemented main to accept a url argument

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const { argv } = require('node:process')
+
+function main() {
+    if (argv.length < 3 || argv.length > 3) {
+        console.error('Invalid arguments'
+        + '\nOne and only one argument must be provided, as the base url to crawl'
+        + '\n\tEx. npm start https://google.com')
+        return
+    }
+    const baseURL = argv[2]
+    if (!URL.canParse(baseURL)) {
+        console.error('Invalid URL'
+        + '\nThe URL must be in a format accepted by a browser'
+        + '\n\tEx. https://www.google.com'
+        + '\n\tEx2. http://google.com/support/'
+        + '\n\tEx3. www.google.com')
+        return
+    }
+    console.log(`Running web crawler at ${baseURL}`)
+}
+
+main()
